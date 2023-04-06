66bdd2c5bee1709d5a94c359b2db877f7dfe6abc
Mai-Lapyst <info@lapyst.by>
2023-04-07 01:23:09 +0200

    Add module async.main with templates to make an async main

---
+++ source/async/main.d
@@ -0,0 +1,60 @@
+module async.main;
+
+/// Global to store the exit code for the asyncronous main
+int g_async_exitcode = 0;
+
+/// Global to store the commandline arguments for the asyncronous main
+string[] g_async_args;
+
+/**
+ * Template to declare an asyncronous main.
+ *
+ * Example:
+ * ---
+ * import async;
+ * mixin AsyncMain!"my_main";
+ * int my_main(string[] args) {
+ *     import core.time : seconds;
+ *     timeout(seconds(5)).await();
+ *     return 0;
+ * }
+ * ---
+ * 
+ * Params:
+ *  func = the function to use as asyncronous main
+ */
+template AsyncMain(string func = "async_main", bool kill_scheduler_on_exit = true) {
+    mixin AsyncLoop!(func, kill_scheduler_on_exit);
+
+    int main(string[] args) {
+        return mainAsyncLoop(args);
+    }
+}
+
+template AsyncLoop(string func, bool kill_scheduler_on_exit) {
+    import std.meta : AliasSeq;
+    import std.traits : isFunction, ReturnType, Parameters;
+
+    alias __fun = mixin(func);
+    static assert (isFunction!__fun, "`" ~ __traits(identifier, __fun) ~ "` needs to be a function");
+    static assert (
+        is(ReturnType!__fun == int),
+        "`" ~ __traits(identifier, __fun) ~ "` is " ~ ReturnType!__fun.toString ~ " but should be `int` instead"
+    );
+    static if (is(Parameters!__fun == AliasSeq!(string[]))) {
+        void asyncMainWrapper() { g_async_exitcode = __fun(g_async_args); }
+    }
+    else static if (is(Parameters!__fun == AliasSeq!())) {
+        void asyncMainWrapper() { g_async_exitcode = __fun(); }
+    }
+    else {
+        static assert(0, "`" ~ __traits(identifier, __fun) ~ "` needs to be callable with either no arguments or only with `string[]`");
+    }
+
+    int mainAsyncLoop(string[] args) {
+        g_async_args = args;
+        gscheduler.schedule(&asyncMainWrapper);
+        gscheduler.loop();
+        return g_async_exitcode;
+    }
+}
