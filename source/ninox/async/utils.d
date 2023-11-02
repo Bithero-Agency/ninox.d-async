0c60299e9cb88844bc0003a3f2adaecda61dd931
Mai-Lapyst <info@lapyst.by>
2023-11-02 06:52:20 +0100

    Remove ninox.async.utils.Option

--- source/ninox/async/utils.d
+++ source/ninox/async/utils.d
@@ -1,46 +1,5 @@
 module ninox.async.utils;
 
-class OptionIsNoneException : Exception {
-    this (string s, string op) {
-        super("Option " ~ s ~ " is none; operation " ~ op ~ " not permitted");
-    }
-}
-
-struct Option(T) {
-    private T value;
-    private bool _isSome = false;
-
-    this(T value, bool isSome) @disable;
-
-    private this(T value) {
-        this.value = value;
-        this._isSome = true;
-    }
-
-    T take() {
-        if (!_isSome) {
-            throw new OptionIsNoneException(.stringof, ".take()");
-        }
-        return value;
-    }
-
-    bool isNone() {
-        return !_isSome;
-    }
-
-    bool isSome() {
-        return _isSome;
-    }
-
-    static Option none() {
-        return Option();
-    }
-
-    static Option some(T value) {
-        return Option(value);
-    }
-}
-
 template MakeClosure() {
     import std.traits : ReturnType, isFunction;
     C makeClosure(
